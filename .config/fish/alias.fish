3e1e0b62cfa5af5ad8ef8bf11f04fa2204e26abf
HumanEntity <human_entity@icloud.com>
2024-02-09 21:09:30 +0100

    Fish config

---
+++ .config/fish/alias.fish
@@ -0,0 +1,84 @@
+
+if type -q exa
+	alias ls "exa --icons --group-directories-first"
+	alias la "exa --icons --group-directories-first -a"
+	alias ll "la -lh"
+	alias lg "ll --git"
+	alias l. "exa -a | egrep '^\.'"
+else
+	alias la "ls -a"
+	alias ll "la -lh"
+	alias lg "ll"
+	alias l. "la | egrep '^\.'"
+end
+
+alias grep "grep --color=auto"
+alias egrep "egrep --color=auto"
+alias fgrep "fgrep --color=auto"
+alias sed "gsed"
+
+if type -q vim
+	alias "vi" "vim --clean"
+end
+# alias "vim" "nvim"
+# if type -q hx 
+# 	alias "nvim" "hx"
+# end
+if type -q helix
+	alias "hx" "helix"
+end
+
+if type -q lazygit
+	alias lg "lazygit"
+end
+
+alias .. "cd .."
+alias ... "cd ../.."
+alias .2 "cd ../.."
+alias .3 "cd ../../.."
+alias .4 "cd ../../../.."
+alias .5 "cd ../../../../.."
+
+alias cp "cp -i"
+# alias lynx "lynx -vikeys"
+if type -q bat
+	alias cat="bat --plain"
+end
+
+if type -q ranger
+	alias r "ranger"
+end
+
+# alias min "minimal_fish"
+alias reload "exec fish"
+
+# alias cwr "cargo watch -q -c -x 'run'"
+
+alias install "nix-env -i"
+alias remove "nix-env -e"
+alias clean "nix-collect-garbage; nix-store --optimise"
+
+if type -q git
+	alias g "git"
+	alias gs "git status"
+	alias gc "git commit"
+	alias gl "git log --oneline"
+	alias ga "git add"
+	alias gaa "git add ."
+	alias config 'git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
+end
+
+if type -q tig
+	alias ti="TERM=screen-256color tig"
+	alias gi="TERM=screen-256color tig"
+end
+if type -q htop
+	alias htop="TERM=screen-256color $(which htop)"
+end
+
+alias josh="joshuto --change-directory"
+
+alias demacs="emacs --with-profile doom"
+alias space="emacs --with-profile space"
+
+alias rr='curl -s -L https://raw.githubusercontent.com/keroserene/rickrollrc/master/roll.sh | bash'
---
+++ .config/fish/path.fish
@@ -0,0 +1,73 @@
+set PATH $PATH $HOME/.local/share/bob/nightly/nvim-macos/bin/
+set PATH $PATH $HOME/bin/
+set PATH $PATH /Applications/Docker.app/Contents/Resources/bin/
+set PATH $PATH $HOME/.config/tmux/plugins/tmuxifier/bin/
+
+set PATH $PATH $HOME/.night.zig/nz/
+set PATH $PATH $HOME/.night.zig/latest/
+
+set -x HELIX_RUNTIME ~/dev/tools/helix/runtime
+# set -x NIX_PATH "$HOME/.nix-defexpr/channels:nixpkgs=$HOME/.nix-defexpr/channels/unstable"
+# bash -c "export NIX_PATH ${NIX_PATH:+$NIX_PATH:}$HOME/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels"
+# Pyenv
+# set PATH $PATH ~/.pyenv/bin/
+set -Ux PYENV_ROOT $HOME/.pyenv
+fish_add_path $PYENV_ROOT/bin
+# Scripts
+set PATH ~/.scripts/ $PATH
+set PATH ~/.scripts/colorscripts/ $PATH
+# Brew
+set PATH /opt/homebrew/bin/ $PATH
+set PATH $PATH /opt/homebrew/opt/llvm/bin
+# DOOM Emacs
+set PATH $PATH ~/.config/doom-emacs/bin/
+# GNU EMACS
+set PATH "~/.emacs.d/bin" $PATH
+# RUST
+set PATH $PATH ~/.cargo/bin/
+
+# Firefox
+set PATH $PATH /Applications/Firefox.app/Contents/MacOS
+
+# GO
+set -gx GOROOT /usr/local/go
+set PATH $PATH $GOROOT/bin
+
+set -gx GOPATH ~/.golib
+set PATH $PATH $GOPATH/bin
+# set -p PATH $PATH /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin
+
+# PYENV Config
+# set PYENV_ROOT "$HOME/.pyenv"
+# command -v pyenv > /dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
+# eval "$(pyenv init -)"
+set PATH /usr/local/opt/tcl-tk/bin $PATH
+
+# set -gx LIBRARY_PATH $LIBRARY_PATH $( brew --prefix )/lib:$(brew --prefix)/opt/libiconv/lib
+# set -gx LIBRARY_PATH $LIBRARY_PATH /usr/lib/ /usr/local/lib/ /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/System/Library/Frameworks/ /opt/homebrew/lib/
+
+# /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/lib/ /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/System/Library/Frameworks/ 
+
+# set -gx LDFLAGS $LDFLAGS -F /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/System/Library/Frameworks/Security.framework/
+set -gx LDFLAGS $LDFLAGS "-L/usr/local/lib -lobjc -I/opt/homebrew/include"
+
+# set -x PATH $PATH /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/
+set -x PATH $PATH /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/bin/
+set -x PATH $PATH /Library/Frameworks/
+
+set -x LDFLAGS $LDFLAGS "-F/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/System/Library/Frameworks/ -F/Library/Frameworks/ -F/System/Library/Frameworks/"
+set -x LDFLAGS $LDFLAGS "-L/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/System/Library/Frameworks/ -L/Library/Frameworks/ -L/System/Library/Frameworks/"
+set -x CFLAGS $CFLAGS "-F/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/System/Library/Frameworks/ -F/Library/Frameworks/ -F/System/Library/Frameworks/"
+# set -x CPATH $CPATH "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include/"
+
+
+set -x LDFLAGS $LDFLAGS "-L/opt/homebrew/opt/libiconv/lib"
+set -x CFLAGS $CFLAGS "-I/opt/homebrew/opt/libiconv/include"
+set -x CFLAGS $CFLAGS "-I/opt/homebrew/Cellar/libmagic/5.45/include/magic.h"
+
+set -x PATH $PATH ~/.local/bin
+set -x PATH $PATH ~/dev/v/v/
+
+set -x MANPATH $MANPATH /opt/homebrew/opt/gh/share/man/
+
+set -x PATH $PATH ~/.config/v-analyzer/bin/
