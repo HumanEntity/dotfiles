3333dd32044dbc529fe301f9213de15179dd42de
HumanEntity <human_entity@icloud.com>
2025-12-09 17:22:58 +0100

    Some wezterm stuff

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,119 +1,51 @@
 local wezterm = require("wezterm")
 local config = {}
-local smart_splits = wezterm.plugin.require("https://github.com/mrjones2014/smart-splits.nvim")
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
 
-require("multiplexing").setup(config)
-require("launch").setup(config)
-
--- smart_splits.apply_to_config(config, {
--- 	-- the default config is here, if you'd like to use the default keys,
--- 	-- you can omit this configuration table parameter and just use
--- 	-- smart_splits.apply_to_config(config)
-
--- 	-- directional keys to use in order of: left, down, up, right
--- 	direction_keys = { "h", "j", "k", "l" },
--- 	-- modifier keys to combine with direction_keys
--- 	modifiers = {
--- 		move = "CTRL", -- modifier to use for pane movement, e.g. CTRL+h to move left
--- 		resize = "META", -- modifier to use for pane resize, e.g. META+h to resize to the left
--- 	},
--- })
+-- require("multiplexing").setup(config)
+-- require("launch").setup(config)
 
 config.font = wezterm.font("JetBrainsMono Nerd Font")
--- config.color_scheme = "Horizon Dark (Gogh)"
-config.color_scheme = "Gnometerm (terminal.sexy)"
-config.font_size = 18
+-- config.font = wezterm.font("RobotoMono Nerd Font")
+-- config.font = wezterm.font("FiraCode Nerd Font")
+-- config.font = wezterm.font("Iosevka Nerd Font")
+-- config.font = wezterm.font("Terminess Nerd Font")
+-- config.font = wezterm.font("BlexMono Nerd Font")
+-- config.font = wezterm.font("CaskaydiaCove Nerd Font")
+-- config.color_scheme = "Adventure"
+-- config.color_scheme = "Gruvbox dark, hard (base16)"
+-- config.color_scheme = "MaterialOcean"
+-- config.color_scheme = "Solarized Dark - Patched"
+-- config.color_scheme = "Tokyo Night Storm"
+
+config.colors = {}
+-- config.colors.background = "#111111"
+config.colors.background = "black"
+
+config.window_background_opacity = 0.9
+config.window_decorations = "RESIZE"
+config.font_size = 17.5
+
+config.window_background_opacity = 0.95
+config.macos_window_background_blur = 50
 
 -- Tab bar
+config.enable_tab_bar = false
 
-config.use_fancy_tab_bar = false
-config.tab_max_width = 32
-config.colors = {
-	tab_bar = {
-		-- The color of the strip that goes along the top of the window
-		-- (does not apply when fancy tab bar is in use)
-		background = "#0b0022",
-
-		-- The active tab is the one that has focus in the window
-		active_tab = {
-			-- The color of the background area for the tab
-			bg_color = "#2b2042",
-			-- The color of the text for the tab
-			fg_color = "#c0c0c0",
-
-			-- Specify whether you want "Half", "Normal" or "Bold" intensity for the
-			-- label shown for this tab.
-			-- The default is "Normal"
-			intensity = "Normal",
-
-			-- Specify whether you want "None", "Single" or "Double" underline for
-			-- label shown for this tab.
-			-- The default is "None"
-			underline = "None",
-
-			-- Specify whether you want the text to be italic (true) or not (false)
-			-- for this tab.  The default is false.
-			italic = false,
-
-			-- Specify whether you want the text to be rendered with strikethrough (true)
-			-- or not for this tab.  The default is false.
-			strikethrough = false,
-		},
-
-		-- Inactive tabs are the tabs that do not have focus
-		inactive_tab = {
-			bg_color = "#1b1032",
-			fg_color = "#808080",
-
-			-- The same options that were listed under the `active_tab` section above
-			-- can also be used for `inactive_tab`.
-		},
-
-		-- You can configure some alternate styling when the mouse pointer
-		-- moves over inactive tabs
-		inactive_tab_hover = {
-			bg_color = "#3b3052",
-			fg_color = "#909090",
-			italic = true,
-
-			-- The same options that were listed under the `active_tab` section above
-			-- can also be used for `inactive_tab_hover`.
-		},
-
-		-- The new tab button that let you create new tabs
-		new_tab = {
-			bg_color = "#1b1032",
-			fg_color = "#808080",
-
-			-- The same options that were listed under the `active_tab` section above
-			-- can also be used for `new_tab`.
-		},
-
-		-- You can configure some alternate styling when the mouse pointer
-		-- moves over the new tab button
-		new_tab_hover = {
-			bg_color = "#3b3052",
-			fg_color = "#909090",
-			italic = true,
-
-			-- The same options that were listed under the `active_tab` section above
-			-- can also be used for `new_tab_hover`.
-		},
-	},
-}
-
-config.window_background_opacity = 0.99
--- config.window_background_image = "/Users/tomek/Documents/Wallpapers/wallpaper-leaf.jpg"
+-- config.window_background_opacity = 0.99
+--config.window_background_image = "/Users/tomek/Documents/Wallpapers/simple.jpg"
 config.window_background_image_hsb = {
-	-- Darken the background image by reducing it to 1/3rd
-	brightness = 0.1,
+  -- Darken the background image by reducing it to 1/3rd
+  brightness = 0.1,
 
-	-- You can adjust the hue by scaling its value.
-	-- a multiplier of 1.0 leaves the value unchanged.
-	hue = 0.5,
+  -- You can adjust the hue by scaling its value.
+  -- a multiplier of 1.0 leaves the value unchanged.
+  -- hue = 0.5,
 
-	-- You can adjust the saturation also.
-	saturation = 0.5,
+  -- You can adjust the saturation also.
+  saturation = 0.5,
 }
 
 return config
